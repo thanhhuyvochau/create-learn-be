8670a77f4e3aa80250d48749cf2d31a38f7ab8f7
thanhhuyvochau <thanhhuyvochau@gmail.com>
2025-10-09 18:55:22 +0700

    remove reduntant method query of ClassRepository

--- src/main/java/org/project/createlearnbe/repositories/ClazzRepository.java
+++ src/main/java/org/project/createlearnbe/repositories/ClazzRepository.java
@@ -14,14 +14,6 @@ import java.util.Optional;
 
 public interface ClazzRepository extends JpaRepository<Clazz, Long> {
   Optional<Clazz> findClazzByIsDisplayedAndId(Boolean isDisplayed, Long id);
-
-  Page<Clazz> findAllByIsDisplayed(Boolean isDisplayed, Pageable pageable);
-
-  Page<Clazz> findAllByIsDisplayedAndPriceEquals(
-      Boolean isDisplayed, BigDecimal price, Pageable pageable);
-
-  Page<Clazz> findTopByIsDisplayedOrderByClickCountDesc(Boolean isDisplayed, Pageable pageable);
-
   @Query(
       """
           SELECT c FROM Clazz c
