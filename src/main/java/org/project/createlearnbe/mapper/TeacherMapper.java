0addc336164e41d8b2593333397a4dc136df64d7
thanhhuyvochau <thanhhuyvochau@gmail.com>
2025-10-26 13:27:13 +0700

    add absolute path builder for teacher image

--- src/main/java/org/project/createlearnbe/mapper/TeacherMapper.java
+++ src/main/java/org/project/createlearnbe/mapper/TeacherMapper.java
@@ -1,16 +1,30 @@
 package org.project.createlearnbe.mapper;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.project.createlearnbe.dto.request.TeacherRequestDto;
 import org.project.createlearnbe.dto.response.TeacherResponseDto;
 import org.project.createlearnbe.entities.Teacher;
+import org.project.createlearnbe.utils.UrlUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
-public interface TeacherMapper {
-    TeacherResponseDto toDto(Teacher teacher);
+@Mapper(componentModel = "spring", uses = {UrlUtils.class})
+public abstract class TeacherMapper {
 
-    Teacher toEntity(TeacherRequestDto dto);
+    @Autowired
+    protected UrlUtils urlUtils;
 
-    void updateEntityFromDto(TeacherRequestDto dto, @MappingTarget Teacher teacher);
+    public abstract TeacherResponseDto toDto(Teacher teacher);
+
+    public abstract Teacher toEntity(TeacherRequestDto dto);
+
+    public abstract void updateEntityFromDto(TeacherRequestDto dto, @MappingTarget Teacher teacher);
+
+    @AfterMapping
+    protected void afterMapping(Teacher entity, @MappingTarget TeacherResponseDto dto) {
+        dto.setProfileImageUrl(
+                urlUtils.buildAbsolutePath(entity.getProfileImageUrl())
+        );
+    }
 }
