2f8a5cdccca534792e38e8cc6999d0162cc55af1
thanhhuyvochau <thanhhuyvochau@gmail.com>
2026-03-03 21:27:30 +0700

    fix issue updating then lose image

--- src/main/java/org/project/createlearnbe/utils/UrlUtils.java
+++ src/main/java/org/project/createlearnbe/utils/UrlUtils.java
@@ -15,4 +15,24 @@ public class UrlUtils {
     relativePath = relativePath.startsWith("/") ? relativePath.substring(1) : relativePath;
     return String.format("%s/%s", minioProperties.getExternalUrl(), relativePath);
   }
+
+  // This method help strip the external URL from the given path, returning only the relative path.
+  // If the external URL is not found in the path, it returns the original path.
+  // USE CASE: when set up https for minio, the external URL will be https://domain.com/resource,
+  // BUT
+  // the path stored in database is /minio/filename, and when update we need to cut the prefix
+  // 'resource' and get the original to avoid mistake write to old record when do updating function
+  public String stripMinioExternalUrl(String path) {
+    String externalUrl = minioProperties.getExternalUrl();
+    if (!path.contains(externalUrl)) {
+      return path;
+    }
+    int endIndex = path.indexOf(externalUrl) + externalUrl.length();
+    String relativePath = path.substring(endIndex);
+    if (!relativePath.startsWith("/")) {
+      relativePath = "/" + relativePath;
+    }
+
+    return relativePath;
+  }
 }
